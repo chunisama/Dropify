77b3dcf1e0a60bd029cc8ab06b84e6d20ab65e5b
“Eugene <eugenechung1209@gmail.com>
2019-10-04 22:54:20 -0400

    finish displaying timers

--- frontend/components/player/player.jsx
+++ frontend/components/player/player.jsx
@@ -3,17 +3,65 @@ import React from "react";
 class Player extends React.Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {
+            duration: null,
+            volume: "1",
+        };
+        this.playAudio = this.playAudio.bind(this);
+        this.pauseAudio = this.pauseAudio.bind(this);
     }
 
     componentDidMount(){
         this.props.fetchSongs();
-        debugger
+        this.handleProgressBar();
+        this.handleVolume();
     }
-    handlePlay() {
+
+    isPlaying(){
+        return !this.audio.paused;    
+    }
+    
+    playAudio() {
         this.audio.play();
     }
 
+    pauseAudio() {
+        this.audio.pause();
+    }
+
+    handleVolume() {
+        this.volume.onchange = (e) => {
+            this.audio.volume = e.target.value;
+        }
+    }
+
+    handleProgressBar() {
+        this.range.value = 0;
+        this.currentTimeInterval = null;
+
+        this.audio.onloadedmetadata = () => {
+            this.setState({duration: this.audio.duration})
+        }
+
+
+        this.audio.onplay = () => {
+            this.currentTimeInterval = setInterval( () => {
+                this.range.value = this.audio.currentTime;
+            }, 500);
+        };
+        
+        this.audio.onpause = () => {
+            clearInterval(this.currentTimeInterval);
+        };
+
+        this.range.onchange = (e) => {
+            clearInterval(this.currentTimeInterval);
+            this.audio.currentTime = e.target.value;
+            this.currentTimeInterval = setInterval(() => {
+                this.range.value = this.audio.currentTime;
+            }, 500);
+        }
+    }
 
     songExists(){
         if (this.props.songs.length >= 1) {
@@ -24,15 +72,16 @@ class Player extends React.Component {
     }
 
     render(){
-        const { songs } = this.props;
-        debugger
         return(
-            <figure>
-                <audio src={this.songExists()}></audio>
-            </figure>
+            <div>
+                <audio ref={(audio) => { this.audio = audio }} src={this.songExists()}></audio>
+                <input type="button" value="Play" onClick={() => this.playAudio()}/>
+                <input type="button" value="Pause" onClick={() => this.pauseAudio()}/>
+                <input type="range" ref={(range) => this.range = range} min="0" max={this.state.duration}/>
+                <input type="range" ref={(volume) => this.volume = volume} min="0" step="0.01" max={this.state.volume}/>
+            </div>
         )       
     }
 }
 
-// { `${songs[0].songUrl}` }
 export default Player;
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,12 +2,14 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import RootReducer from "../reducers/root_reducer";
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 const configureStore = (preloadedState = {}) => {
   return createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    composeWithDevTools(applyMiddleware(thunk, logger)), 
   );
 };
 
