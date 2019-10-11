f7e9e697f9ad4c24a7dcf0d3e1fd7535a044e7f4
“Eugene <eugenechung1209@gmail.com>
2019-10-11 11:09:49 -0400

    fixed syntax error

--- app/controllers/api/playlists_songs_controller.rb
+++ app/controllers/api/playlists_songs_controller.rb
@@ -3,7 +3,6 @@ class Api::PlaylistsSongsController < ApplicationController
         @playlists_songs = PlaylistSongs.find_by(
             playlist_id: params[:playlists_songs][:playlist_id], 
             song_id: params[:playlists_songs][:song_id])
-            )
         @playlists_songs.destroy
         render json: {
             playlist_id: @playlists_songs.playlist_id,
